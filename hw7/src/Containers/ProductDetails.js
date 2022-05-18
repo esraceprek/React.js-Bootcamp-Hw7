2f875f2c112b05c1385f52e7eb787953acea9aa9
esraceprek <esraceprek3@gmail.com>
2022-05-18 13:52:51 +0300

    completed

--- hw7/src/Containers/ProductDetails.js
+++ hw7/src/Containers/ProductDetails.js
@@ -31,7 +31,8 @@ const ProductDetails = () => {
             dispatch(removeSelectedProduct());
 
         };
-    } , [dispatch, fetchProductDetails, productId]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    } , [ productId]);
     return (
         <div className="ui grid container">
             {Object.keys(product).length === 0 ? (
@@ -40,7 +41,7 @@ const ProductDetails = () => {
             
                 <div className="ui placeholder segment">
                   <div className="ui two column stackable center aligned grid">
-                    <div className="ui vertical divider">AND</div>
+                    <div className="ui vertical divider"></div>
                     <div className="middle aligned row">
                       <div className="column lp">
                         <img className="ui fluid image" src={image} alt={title}/>
